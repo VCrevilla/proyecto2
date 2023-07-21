301957d07bdbe5b33a8f9d31badb30af28fefea0
Carol Revilla <carolrevilla2004@gmail.com>
2023-07-21 09:53:18 -0500

    v04 se creo 3 componentes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import C01componente from './component/C01componente';
 import C02contador from './component/C02contador';
 import C03contador from './component/C03contador';
+import C04variable from './component/C04variable';
+import C05operadorTernario from './component/C05operadorTernario';
+import C06matriz from './component/C06matriz';
 
 function App() {
   return (
@@ -10,11 +13,20 @@ function App() {
       <h1>Componente 1</h1>
       <C01componente></C01componente>
       
-      <h2>Componente 2</h2>
+      <h1>Componente 2</h1>
       <C02contador></C02contador>
 
-      <h3>Componente 3</h3>
+      <h1>Componente 3</h1>
       <C03contador></C03contador>
+
+      <h1>Componente 4</h1>
+      <C04variable></C04variable>
+
+      <h1>Componente 5</h1>
+      <C05operadorTernario></C05operadorTernario>
+
+      <h1>Componente 6</h1>
+      <C06matriz/>
 
 
     </div>
---
+++ src/component/C04variable.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const C04variable = (msj) => {
+  return (
+    <div>
+        <div>
+            <h2>Cuarto componente{msj.xVariable}</h2>
+            <h1>Dando lectura a una variable recibida...</h1>
+
+        </div>
+      
+    </div>
+  );
+}
+
+export default C04variable
