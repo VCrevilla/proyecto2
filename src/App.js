657fb015f5aa9f718ba820298d51971ba1bb3e82
Carol Revilla <carolrevilla2004@gmail.com>
2023-08-04 09:30:16 -0500

    impares 7,5,3,1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import C07MatrizOperaciones from './component/C07matrizoperaciones';
 import C05operadorTernario from './component/C05operadorTernario';
 import C03contador from './component/C03contador';
 import C01componente from './component/C01componente';
@@ -9,12 +10,14 @@ import C01componente from './component/C01componente';
 function App() {
   return (
     <div>
+     <h1>componente 07</h1>
+     <C07MatrizOperaciones/>
      <h1>componente 05</h1>
      <C05operadorTernario/>
      <h1>componente 03</h1>
      <C03contador/>
      <h1>componente 1</h1>
-     <C01componente/> 
+     <C01componente/>
      
 
       
