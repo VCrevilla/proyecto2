a85f7212f4baf37a1d93ae00c5963cd8d1314a3e
Carol Revilla <carolrevilla2004@gmail.com>
2023-08-04 08:49:30 -0500

    V07 se modifico para el examen

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import C01componente from './component/C01componente';
-import C02contador from './component/C02contador';
-import C03contador from './component/C03contador';
-import C04variable from './component/C04variable';
-import C05operadorTernario from './component/C05operadorTernario';
 import C06matriz from './component/C06matriz';
+import C05operadorTernario from './component/C05operadorTernario';
+import C04variable from './component/C04variable';
+import C03contador from './component/C03contador';
+import C02contador from './component/C02contador';
+
+
 
 function App() {
   return (
     <div>
-      <h1>Componente 1</h1>
-      <C01componente></C01componente>
       
-      <h1>Componente 2</h1>
-      <C02contador></C02contador>
-
-      <h1>Componente 3</h1>
-      <C03contador></C03contador>
-
-      <h1>Componente 4</h1>
-      <C04variable></C04variable>
-
-      <h1>Componente 5</h1>
-      <C05operadorTernario></C05operadorTernario>
-
-      <h1>Componente 6</h1>
+      <h1>componente 06</h1>
       <C06matriz/>
+      <h1>componente 05</h1>
+      <C05operadorTernario/>
+      <h1>componente 04</h1>
+      <C04variable/>
+      <h1>componente 03</h1>
+      <C03contador/>
+      <h1>componente 02</h1>
+      <C02contador/>
+      <h1>componente 1</h1>
+      <C01componente/>
+      
+      
+      
 
 
     </div>
