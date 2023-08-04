8777bcf6c9337e860ba3d428d9919bd85da0812a
Carol Revilla <carolrevilla2004@gmail.com>
2023-08-04 08:56:12 -0500

    solo impares de abajo asi a arriba

--- src/App.js
+++ src/App.js
@@ -1,32 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import C01componente from './component/C01componente';
-import C06matriz from './component/C06matriz';
 import C05operadorTernario from './component/C05operadorTernario';
-import C04variable from './component/C04variable';
 import C03contador from './component/C03contador';
-import C02contador from './component/C02contador';
+import C01componente from './component/C01componente';
 
 
 
 function App() {
   return (
     <div>
-      
-      <h1>componente 06</h1>
-      <C06matriz/>
-      <h1>componente 05</h1>
-      <C05operadorTernario/>
-      <h1>componente 04</h1>
-      <C04variable/>
-      <h1>componente 03</h1>
-      <C03contador/>
-      <h1>componente 02</h1>
-      <C02contador/>
-      <h1>componente 1</h1>
-      <C01componente/>
-      
-      
+     <h1>componente 05</h1>
+     <C05operadorTernario/>
+     <h1>componente 03</h1>
+     <C03contador/>
+     <h1>componente 1</h1>
+     <C01componente/> 
+     
+
       
 
 
